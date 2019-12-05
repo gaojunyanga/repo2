8cc6a5fb795e5116c5d41b1b5da39047a8b1e658
gaojunyanga <524840158@qq.com>
2019-12-05 16:12:07 +0800

    添加工程到本地仓库

---
+++ test-git/src/main/java/com/gll/gitdemo/HelloGit.java
@@ -0,0 +1,14 @@
+package com.gll.gitdemo;
+
+/**
+ * @ClassName HelloGit
+ * @Author glls
+ * @Date 2019-12-05 16:06
+ * @Version 1.0
+ * @Description TODO
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
